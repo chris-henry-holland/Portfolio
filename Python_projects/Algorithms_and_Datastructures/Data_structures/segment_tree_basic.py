69428bc59a99bda9c966a322c8482422d2550dcd
chris-henry-holland <chris.henry.holland@gmail.com>
2024-11-21 12:57:46 +0000

    Updated the documentation of the SegmentTree class

--- Python_projects/Algorithms_and_Datastructures/Data_structures/segment_tree_basic.py
+++ Python_projects/Algorithms_and_Datastructures/Data_structures/segment_tree_basic.py
@@ -6,36 +6,56 @@ class SegmentTree(object):
     
     """
     Creates a segment tree for an integer sequence based around
-    specified assoicative binary operations.
+    a specified associative binary operation.
+
+    On initialization, the sequence is entirely populated with the
+    identity element of the given binary operation
     
-    Args:
+    Initialization args:
         Required positional:
         mn (int): Lower bound for sequence index values
         mx (int): Upper bound for sequence index values
         
         Optional named:
-        ops (dict): Specifies the binary operation(s) to be considered.
-                Given as a dictionary whose keys are unique strings
-                with corresponding value either a 2-tuple whose 0th
-                index is the associative binary operation (as a
-                function) and whose 1st index is the identity of that
-                binary operation, or a string representing
-                a standard binary operation. The standard binary
-                operations implemented are "sum" (gives the interval
-                sums- identity 0), "product" (gives the inteval
-                product- identity 1), "max" (gives the interval
-                maximum), "min" (gives the interval minimum) and
-                "union" (gives the union of sets on the interval).
-                Note- it is possible to use the same binary operation
-                multiple times (as long as each has a different key),
-                which can be useful in the case where different
-                iterations through the sequence result in different
-                values for the same element (see example below)
-            Default: {"sum": "sum"}, or equivalently:
+        op (string 2-tuple): Specifies the associative binary
+                operation to be applied and its identity, either as
+                a string identifying a standard binary operation or
+                a 2-tuple giving the binary operation at index 0
+                and the identity at index 1.
+                The standard binary operations implemented are:
+                 "sum" (gives the interval sums for real numeric values-
+                    identity 0)
+                 "product" (gives the interval products for
+                    real numeric values- identity 1)
+                 "max" (gives the interval maxima for real numeric
+                    values- identity -float("inf"))
+                 "min" (gives the interval minima for real numeric
+                    values- identity float("inf"))
+                 "union" (gives the union of sets over the intervals
+                    for sets- identity set(), the empty set)
+                 "bitwise and" (gives the interval bitwise and for
+                    integers- identity -1).
+            Default: "sum", or equivalently:
                     {"sum": (lambda x, y: x + y, 0)}
     
     Attributes:
-        
+        min (int): The index of the first term in the sequence
+        max (int): The index of the final term in the sequence
+        op (2-tuple): Contains at index 0 the binary assiciative
+                operation as a function that takes two ordered
+                inputs and gives a single output of the same
+                kind, and at index 1 the identity element of that
+                binary operation.
+        size (int): The number of terms of the sequence (equal to
+                max - min + 1).
+        tree (list): A list of length twice the size representing
+                the segment tree. The second half of this list is
+                the same as the original sequence (in the same order).
+        offset (int): The difference between the position of the first
+                term in the sequence in the attribute tree (i.e. the
+                attribute size) and index of the first term in the
+                sequence (i.e. the attribute min).
+                Consequently this takes the value (size - min).
         
     Methods:
         (See method documentation for specific details)
@@ -43,7 +63,8 @@ class SegmentTree(object):
                 of the binary operations.
         update(): Sets the number associated with a given sequence
                 value for one of the binary operations.
-                
+        populate(): Sets the values for the sequence starting at a
+                given index.
     """
     
     std_ops = {"sum": (lambda x, y: x + y, 0),
@@ -63,11 +84,38 @@ class SegmentTree(object):
         self.tree = [self.op[1] for _ in range(2 * self.size)]
     
     def __getitem__(self, i: int) -> Optional[Any]:
+        """
+        Returns the term in the sequence corresponding to the
+        index i, if there is such a term, otherwise None.
+        """
         if i < self.min or i > self.max:
             return None
         return self.tree[i + self.offset]
 
     def query(self, l: int, r: int) -> Any:
+        """
+        For the contiguous subsequence with indices no less than l
+        and no greater than r, returns the result of repeatedly
+        replacing the first and second terms of the subsequence with
+        the result of the binary operation of the first term with the
+        second term (in that order) until a single term remains.
+
+        If the subsequence is empty (e.g. if l > r or the range [l, r]
+        is outside the range of valid indices), then returns the
+        identity of the binary operation.
+
+        Args:
+            Required positional:
+            l (int): The smallest possible index of the subsequence.
+            r (int): The largest possible index of the subsequence.
+        
+        Returns:
+        Value of the same type as that of the terms in the sequence,
+        representing result the application of the binary operation in
+        the specified subsequence with indices between l and r inclusive
+        as described above if the subsequence is not empty, otherwise
+        the identity element of the binary operation.
+        """
         l = max(l, self.min) + self.offset
         r = min(r, self.max) + self.offset
         
@@ -84,8 +132,30 @@ class SegmentTree(object):
         return res
     
     def update(self, i: int, val: Any) -> None:
+        """
+        Changes the value of the term in the sequence with index i to
+        the value val, updating the rest of the segment tree to
+        reflect this change.
+
+        Args:
+            Required positional:
+            i (int): The index of the sequence term to be changed.
+                    In order for a change to be made, this must be
+                    an integer between the attributes min and max
+                    inclusive.
+            val (any): A value of the same type as that of the terms
+                    in the sequence (i.e. one that can be used as
+                    either the first or second argument in the binary
+                    operation alongside another term) that should
+                    replace the existing term at index i in the
+                    sequence.
+        
+        Returns:
+        None
+        """
         if i < self.min or i > self.max: return
         i += self.offset
+        if self.tree[i] == val: return
         self.tree[i] = val
         while i > 1:
             self.tree[i >> 1] = self.op[0](self.tree[i], self.tree[i ^ 1])
@@ -93,6 +163,36 @@ class SegmentTree(object):
         return
     
     def populate(self, i0: int, arr: List[Any]) -> None:
+        """
+        Overwrites part or all of the sequence in a contiguous block
+        starting at index i0 with the sequence represented by the array
+        arr, in such a way that the number of terms in the sequence does
+        not change.
+        Any terms in arr that end up corresponding to indices outside
+        of the range [min, max] will not be used.
+
+        Args:
+            Required positional:
+            i (int): The index of the first term in the sequence to be
+                    replaced.
+                    In order for a change to be made, this must be
+                    an integer between the attributes min and max
+                    inclusive.
+            arr (list of any type): An ordered list of values of
+                    the same type as that of the terms in the sequence
+                    (i.e. values that can be used as either the first
+                    or second argument in the binary operation alongside
+                    one another or another term) that should replace the
+                    terms in the sequence as a contiguous block starting
+                    from the term with index i0.
+                    I.e. arr[0] replaces the term with index i0, arr[1]
+                    replaces the term with index i0 + 1, ... arr[k]
+                    replaces the term with index i0 + k for integer k
+                    between 0 and len(arr) - 1 inclusive.
+        
+        Returns:
+        None
+        """
         if i0 < self.min:
             arr = arr[self.min - i0:]
         i0 += self.offset
@@ -101,12 +201,9 @@ class SegmentTree(object):
         l = i0
         r = l + len(arr)
         while l > 1:
-            #print(f"l = {l}")
-            #print(self.tree)
             for i in range(l, r):
                 i2 = i >> 1
                 self.tree[i2] = self.op[0](self.tree[i], self.tree[i ^ 1])
-                #print(f"i2 = {i2}")
             l >>= 1
             r = ((r + 1) >> 1)
         return
