730a933ee206d258e83554656c1eae28f0ca0d5d
chris-henry-holland <chris.henry.holland@gmail.com>
2025-05-09 23:25:43 +0100

    Added documentation to the function numberMindExact(), an alternative non-viable solution to Project Euler #185.

--- Python_projects/Project_Euler/Project_Euler_151_200.py
+++ Python_projects/Project_Euler/Project_Euler_151_200.py
@@ -6097,89 +6097,6 @@ def latticeTrianglesContainingOriginCount(lattice_radius: int=105, incl_edge: bo
 
 
 # Problem 185
-def numberMindExact(alphabet: str="0123456789", guesses: List[Tuple[str, int]]=[
-    ("5616185650518293", 2),
-    ("3847439647293047", 1),
-    ("5855462940810587", 3),
-    ("9742855507068353", 3),
-    ("4296849643607543", 3),
-    ("3174248439465858", 1),
-    ("4513559094146117", 2),
-    ("7890971548908067", 3),
-    ("8157356344118483", 1),
-    ("2615250744386899", 2),
-    ("8690095851526254", 3),
-    ("6375711915077050", 1),
-    ("6913859173121360", 1),
-    ("6442889055042768", 2),
-    ("2321386104303845", 0),
-    ("2326509471271448", 2),
-    ("5251583379644322", 2),
-    ("1748270476758276", 3),
-    ("4895722652190306", 1),
-    ("3041631117224635", 3),
-    ("1841236454324589", 3),
-    ("2659862637316867", 2),
-]) -> List[str]:
-
-    # Review- try finding ways to prune the search space to 
-    # make this method viable
-
-    since = time.time()
-    n = len(guesses[0][0])
-
-    f_sets = [set() for _ in range(max(x[1] for x in guesses) + 1)]
-    l_dicts = [{} for _ in range(n)]
-    f_lst = []
-    for i, (s, f) in enumerate(guesses):
-        f_sets[f].add(i)
-        for idx in range(n):
-            l = s[idx]
-            l_dicts[idx].setdefault(l, [])
-            l_dicts[idx][l].append(i)
-        f_lst.append(f)
-    #print(f_sets)
-    #print(l_dicts)
-    
-    curr = []
-    res = []
-
-    def recur(idx: int=n - 1) -> None:
-        if idx < 0:
-            res.append("".join(curr[::-1]))
-            return
-        if len(f_sets) > idx + 1 and f_sets[idx + 1]:
-            opts = {guesses[i][0][idx] for i in f_sets[idx + 1]}
-            if len(opts) > 1: return
-        else: opts = set(alphabet)
-        if f_sets:
-            for i in f_sets[0]:
-                opts.discard(guesses[i][0][idx])
-        if not opts: return
-        curr.append(None)
-        #print(curr, opts)
-        for l in opts:
-            curr[-1] = l
-            shifted = []
-            for i in l_dicts[idx].get(l, []):
-                f = f_lst[i]
-                f_sets[f].remove(i)
-                f_sets[f - 1].add(i)
-                shifted.append(i)
-                f_lst[i] -= 1
-            recur(idx - 1)
-            for i in shifted:
-                f = f_lst[i]
-                f_lst[i] += 1
-                f_sets[f].remove(i)
-                f_sets[f + 1].add(i)
-        curr.pop()
-        return
-    
-    recur(idx=n - 1)
-    print(f"Time taken = {time.time() - since:.4f} seconds")
-    return res
-
 def numberMindSimulatedAnnealing(alphabet: str="0123456789", n_trials: int=20, guesses: List[Tuple[str, int]]=[
     ("5616185650518293", 2),
     ("3847439647293047", 1),
@@ -6268,8 +6185,9 @@ def numberMindSimulatedAnnealing(alphabet: str="0123456789", n_trials: int=20, g
                     ]
     
     Returns:
-    A string (str) with the same length as each of the strings in guesses
-    that is consistent with all of the number of matches with all of the
+    A string (str) consisting of characters in alphabet, with the string
+    having the same length as each of the strings in guesses that is
+    consistent with all of the number of matches with all of the
     strings in guesses. Note that while this may be the original string,
     it is not guaranteed.
     """
@@ -6340,6 +6258,151 @@ def numberMindSimulatedAnnealing(alphabet: str="0123456789", n_trials: int=20, g
                 print(f"new best = {d}")
     
     res = "".join(decode(x) for x in curr)
+    print(f"Time taken = {time.time() - since:.4f} seconds")
+    return res
+
+def numberMindExact(alphabet: str="0123456789", guesses: List[Tuple[str, int]]=[
+    ("5616185650518293", 2),
+    ("3847439647293047", 1),
+    ("5855462940810587", 3),
+    ("9742855507068353", 3),
+    ("4296849643607543", 3),
+    ("3174248439465858", 1),
+    ("4513559094146117", 2),
+    ("7890971548908067", 3),
+    ("8157356344118483", 1),
+    ("2615250744386899", 2),
+    ("8690095851526254", 3),
+    ("6375711915077050", 1),
+    ("6913859173121360", 1),
+    ("6442889055042768", 2),
+    ("2321386104303845", 0),
+    ("2326509471271448", 2),
+    ("5251583379644322", 2),
+    ("1748270476758276", 3),
+    ("4895722652190306", 1),
+    ("3041631117224635", 3),
+    ("1841236454324589", 3),
+    ("2659862637316867", 2),
+]) -> List[str]:
+    """
+    Alternative solution to Project Euler #185, giving all possible solutions
+    rather than just one possible solution.
+
+    Using a given set of characters (alphabet), a string of a given length has
+    been constructed. A sequence of similarly constructed strings are given
+    (each with the same length and using the same set of characters), each having
+    an associated number corresponding to the number of positions in the string
+    for which the character in that position in the string is the same as the
+    character at the equivalent position in the original string (referred to
+    as the number of matches).
+
+    This function produces a list of all of the strings that could have been the
+    original string based on that sequence of similarly constructed strings with
+    the associated number.
+
+    This is currently not a viable solution for the specific inputs of Project
+    Euler #185 (the default inputs) as it has too large a search space.
+
+    Args:
+        Optional named:
+        alphabet (str): A string containing the characters that may be used
+                in the original string.
+            Default: "0123456789"
+        guesses (list of 2-tuples whose index 0 contains a string and whose index
+                1 contains an int): The sequence of strings with the same length
+                as the original string and consisting of the characters in
+                alphabet, each with the corresponding number of matches, with
+                which the returned string must be consistent for all of the
+                contained strings and number of matches.
+            Default: [
+                        ("5616185650518293", 2),
+                        ("3847439647293047", 1),
+                        ("5855462940810587", 3),
+                        ("9742855507068353", 3),
+                        ("4296849643607543", 3),
+                        ("3174248439465858", 1),
+                        ("4513559094146117", 2),
+                        ("7890971548908067", 3),
+                        ("8157356344118483", 1),
+                        ("2615250744386899", 2),
+                        ("8690095851526254", 3),
+                        ("6375711915077050", 1),
+                        ("6913859173121360", 1),
+                        ("6442889055042768", 2),
+                        ("2321386104303845", 0),
+                        ("2326509471271448", 2),
+                        ("5251583379644322", 2),
+                        ("1748270476758276", 3),
+                        ("4895722652190306", 1),
+                        ("3041631117224635", 3),
+                        ("1841236454324589", 3),
+                        ("2659862637316867", 2),
+                    ]
+    
+    Returns:
+    List of strings (str) with each string consisting of characters in
+    alphabet, of the same length as each of the strings in guesses and 
+    is consistent with all of the number of matches with all of the
+    strings in guesses. All such strings are included exactly once, and as
+    such the original string used to give the number of matches for all
+    of the strings in guesses is guaranteed to be in this list.
+    """
+    # Review- try finding ways to prune the search space to 
+    # make this method viable
+
+    since = time.time()
+    n = len(guesses[0][0])
+
+    f_sets = [set() for _ in range(max(x[1] for x in guesses) + 1)]
+    l_dicts = [{} for _ in range(n)]
+    f_lst = []
+    for i, (s, f) in enumerate(guesses):
+        f_sets[f].add(i)
+        for idx in range(n):
+            l = s[idx]
+            l_dicts[idx].setdefault(l, [])
+            l_dicts[idx][l].append(i)
+        f_lst.append(f)
+    #print(f_sets)
+    #print(l_dicts)
+    
+    curr = []
+    res = []
+
+    def recur(idx: int=n - 1) -> None:
+        if idx < 0:
+            res.append("".join(curr[::-1]))
+            return
+        if len(f_sets) > idx + 1 and f_sets[idx + 1]:
+            opts = {guesses[i][0][idx] for i in f_sets[idx + 1]}
+            if len(opts) > 1: return
+        else: opts = set(alphabet)
+        if f_sets:
+            for i in f_sets[0]:
+                opts.discard(guesses[i][0][idx])
+        if not opts: return
+        curr.append(None)
+        #print(curr, opts)
+        for l in opts:
+            curr[-1] = l
+            shifted = []
+            for i in l_dicts[idx].get(l, []):
+                f = f_lst[i]
+                f_sets[f].remove(i)
+                f_sets[f - 1].add(i)
+                shifted.append(i)
+                f_lst[i] -= 1
+            recur(idx - 1)
+            for i in shifted:
+                f = f_lst[i]
+                f_lst[i] += 1
+                f_sets[f].remove(i)
+                f_sets[f + 1].add(i)
+        curr.pop()
+        return
+    
+    recur(idx=n - 1)
     print(f"Time taken = {time.time() - since:.4f} seconds")
     return res
 
